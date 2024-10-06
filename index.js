8958826be359982d6a4b95bf5619fb095fd4b19e
bbenginbestas <bestasbengin@gmail.com>
2024-10-06 20:41:37 +0300

    add random dices to page

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 randomNumber1 = Math.floor(Math.random()*6)+1;
 randomNumber2 = Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png");
